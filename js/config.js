07a5ecfb0200718b8b908409e9bb5b1bfa33b096
EAkash <rebenajui17@gmail.com>
2023-02-25 11:06:46 +0600

    section

---
+++ js/config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        primary_color: "#100f10",
+        secondary_color: "#161616",
+        logo: "#b91d1d",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      backgroundImage: {
+        hero: "url(./img/Batman.jpg)",
+      },
+    },
+  },
+};
